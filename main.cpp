526918e4c46c06e636ed29110d26c1f0232d3731
Гречихин Павел <peve.grechihin@gmail.com>
2023-07-01 14:31:08 +0300

    программа сортировки маленького массива

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
+	int mas[6] = { 5, 9, 2, 7, 11, 6 },
+		tmp;
+
+	for (int i = 0; i < 6; i++)
+	{
+		int index = i;
+
+		for (int j = i+1; j < 6; j++)
+		{
+			if (mas[index] > mas[j])
+			{
+				index = j;
+			}
+		}
+		tmp = mas[i];
+		mas[i] = mas[index];
+		mas[index] = tmp;
+
+		
+	}
+
+	for (int i = 0; i < 6; i++)
+	{
+		cout << mas[i] << " ";
+	}
 
 	return 0;
 }
